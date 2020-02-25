7b77333cad05c8445f31332fe23798fcfa8babdb
Jasmine <jasmine.pilkerton.2022@student.lhschools.org>
2020-02-25 11:56:26 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("my name is ");
+        System.out.println("jasmine");
+        System.out.println("over the summer i hung out with my friends");
+        System.out.println("i visited my family");
+        System.out.println("i went swimming");
+
+    }
+}
+
